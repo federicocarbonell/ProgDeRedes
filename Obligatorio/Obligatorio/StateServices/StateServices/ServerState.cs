7c7b9220ca566c6223922b4b59cb367fae88361a
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-09-15 19:07:11 -0300

    Fix attempt

--- Obligatorio/Obligatorio/StateServices/StateServices/ServerState.cs
+++ Obligatorio/Obligatorio/StateServices/StateServices/ServerState.cs
@@ -69,6 +69,7 @@ namespace StateServices
                 lock (GamesLocker)
                 {
                     _Games = value;
+                    Games = _Games;
                 }
             }
         }
