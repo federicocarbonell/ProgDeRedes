422e4c2c839268efcb60f6a9ca01c4e4b8b2a4bf
Federico Carbonell <fede_carbonell@hotmail.com>
2021-09-18 17:39:40 -0300

    Se agrega la interfaz client handler

---
+++ Obligatorio/Obligatorio/Cliente/Interfaces/IClientHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Client.Interfaces
+{
+    public interface IClientHandler
+    {
+
+        public void AddGame(string title, string genre, string trailer, string cover);
+        public void DeleteGame(int id);
+        public void ModifyGame(int id, string title, string genre, string trailer, string cover);
+        public void QualifyGame(int id, int rating, string review);
+        public void ViewGameDetail(int id);
+        public void ViewGames();
+    }
+}
