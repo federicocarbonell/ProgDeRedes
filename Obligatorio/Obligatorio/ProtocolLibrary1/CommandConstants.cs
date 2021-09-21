9cb1025f958ab00896b03d699365e1f43e16d55b
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-09-21 19:47:56 -0300

    Updated command constants to support all required game operations

--- Obligatorio/Obligatorio/ProtocolLibrary1/CommandConstants.cs
+++ Obligatorio/Obligatorio/ProtocolLibrary1/CommandConstants.cs
@@ -1,9 +1,15 @@
 ﻿namespace ProtocolLibrary
 {
+
     public static class CommandConstants
     {
         public const int Login = 1;
         public const int AddGame = 2;
         public const int GetGames = 3;
+        public const int DeleteGame = 4;
+        public const int ModifyGame = 5;
+        public const int QualifyGame = 6;
+        public const int ViewDetail = 7;
     }
+
 }
