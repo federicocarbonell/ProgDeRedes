33758b41cb8373efd8daf066a7f77c63642ee762
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-09-25 10:25:20 -0300

    Se replica FileTransferProtocol hecho en clase

---
+++ Obligatorio/Obligatorio/ProtocolLibrary1/FileTransferProtocol.cs
@@ -0,0 +1,17 @@
+﻿namespace ProtocolLibrary
+{
+    public static class FileTransferProtocol
+    {
+
+        public const int FileNameSize = 4;
+        public const int FileSize = 8;
+        public const int MaxPacketSize = 8192;
+
+        public static long CalculateParts(long size)
+        {
+            long parts = size / MaxPacketSize;
+            return parts * MaxPacketSize == size ? parts : parts + 1;
+        }
+
+    }
+}
