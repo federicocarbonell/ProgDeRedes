a29d6c8f75269ef2a49dc545cb394bec3cabb0d3
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2021-11-11 09:14:31 -0300

    Added Review service

---
+++ Obligatorio/Obligatorio/StateServer/Services/ReviewService.cs
@@ -0,0 +1,73 @@
+﻿using DTOs;
+using StateServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateServer.Services
+{
+    public class ReviewService : Reviews.ReviewsBase
+    {
+
+        private readonly IRepository<ReviewDTO> ReviewRepository;
+
+        public ReviewService(IRepository<ReviewDTO> reviewRepo)
+        {
+            ReviewRepository = reviewRepo;
+        }
+
+        public Task<AddReviewResponse> AddReview(ReviewMessage message)
+        {
+            try
+            {
+                ReviewRepository.GetInstance().Add(FromMessage(message));
+                return Task.FromResult(new AddReviewResponse
+                {
+                    Message = $"Reviewed game with id {message.GameId} successfully"
+                });
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult(new AddReviewResponse
+                {
+                    Message = e.Message
+                });
+            }
+        }
+
+        public Task<ReviewMessageList> GetReviewsByGameId(GameId message)
+        {
+            try
+            {
+                var reviews = ReviewRepository.GetInstance().GetAll().Where(x => x.GameId == message.Id);
+                var list = new ReviewMessageList();
+
+                foreach(var review in reviews)
+                {
+                    list.Reviews.Add(new ReviewMessage { Id = review.Id, GameId = review.GameId, Rating = review.Rating, Content = review.Content });
+                }
+
+                return Task.FromResult(list);
+            }
+            catch (Exception e)
+            {
+                return Task.FromResult(new ReviewMessageList
+                {
+                });
+            }
+        }
+
+        private ReviewDTO FromMessage(ReviewMessage message)
+        {
+            return new ReviewDTO
+            {
+                Id = message.Id,
+                GameId = message.GameId,
+                Rating = message.Rating,
+                Content = message.Content
+            };
+        }
+
+    }
+}
